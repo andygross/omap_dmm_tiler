ricardon/omap-audio

OMAPDSS: DSI: Allow variable CLKIN divider in DSI PHY
On OMAP3 and OMAP4, the DSI PHY(DDR clock) output was the DSI PLL output divided
by 4. On OMAP5, the default value of this divider is 2, there is a feature of
having the divider as 4 by disabling the Duty Cycle Corrector, but it isn't
power efficient and requires setting of a few extra bits.

Introduce changes to not have a default CLKIN4DDR clock, make it a variable
clock and get the divider using dss_features.

Signed-off-by: Archit Taneja <archit@ti.com>

drivers/video/omap2/dss/dsi.c
@@ -325,6 +325,8 @@ struct dsi_data {
 	unsigned long  fint_min, fint_max;
 	unsigned long lpdiv_max;
 
+	int ddr_div;
+
 	unsigned num_lanes_supported;
 
 	struct dsi_lane_config lanes[DSI_MAX_NR_LANES];
@@ -1173,7 +1175,7 @@ static unsigned long dsi_get_txbyteclkhs(struct platform_device *dsidev)
 {
 	struct dsi_data *dsi = dsi_get_dsidrv_data(dsidev);
 
-	return dsi->current_cinfo.clkin4ddr / 16;
+	return dsi->current_cinfo.clkinxddr / (dsi->ddr_div * 4);
 }
 
 static unsigned long dsi_fclk_rate(struct platform_device *dsidev)
@@ -1296,20 +1298,20 @@ static int dsi_calc_clock_rates(struct platform_device *dsidev,
 	if (cinfo->fint > dsi->fint_max || cinfo->fint < dsi->fint_min)
 		return -EINVAL;
 
-	cinfo->clkin4ddr = 2 * cinfo->regm * cinfo->fint;
+	cinfo->clkinxddr = 2 * cinfo->regm * cinfo->fint;
 
-	if (cinfo->clkin4ddr > 1800 * 1000 * 1000)
+	if (cinfo->clkinxddr > 1800 * 1000 * 1000)
 		return -EINVAL;
 
 	if (cinfo->regm_dispc > 0)
 		cinfo->dsi_pll_hsdiv_dispc_clk =
-			cinfo->clkin4ddr / cinfo->regm_dispc;
+			cinfo->clkinxddr / cinfo->regm_dispc;
 	else
 		cinfo->dsi_pll_hsdiv_dispc_clk = 0;
 
 	if (cinfo->regm_dsi > 0)
 		cinfo->dsi_pll_hsdiv_dsi_clk =
-			cinfo->clkin4ddr / cinfo->regm_dsi;
+			cinfo->clkinxddr / cinfo->regm_dsi;
 	else
 		cinfo->dsi_pll_hsdiv_dsi_clk = 0;
 
@@ -1373,9 +1375,9 @@ retry:
 
 			a = 2 * cur.regm * (cur.clkin/1000);
 			b = cur.regn;
-			cur.clkin4ddr = a / b * 1000;
+			cur.clkinxddr = a / b * 1000;
 
-			if (cur.clkin4ddr > 1800 * 1000 * 1000)
+			if (cur.clkinxddr > 1800 * 1000 * 1000)
 				break;
 
 			/* dsi_pll_hsdiv_dispc_clk(MHz) =
@@ -1384,7 +1386,7 @@ retry:
 					dsi->regm_dispc_max; ++cur.regm_dispc) {
 				struct dispc_clock_info cur_dispc;
 				cur.dsi_pll_hsdiv_dispc_clk =
-					cur.clkin4ddr / cur.regm_dispc;
+					cur.clkinxddr / cur.regm_dispc;
 
 				/* this will narrow down the search a bit,
 				 * but still give pixclocks below what was
@@ -1462,7 +1464,7 @@ int dsi_pll_set_clock_div(struct platform_device *dsidev,
 
 	dsi->current_cinfo.clkin = cinfo->clkin;
 	dsi->current_cinfo.fint = cinfo->fint;
-	dsi->current_cinfo.clkin4ddr = cinfo->clkin4ddr;
+	dsi->current_cinfo.clkinxddr = cinfo->clkinxddr;
 	dsi->current_cinfo.dsi_pll_hsdiv_dispc_clk =
 			cinfo->dsi_pll_hsdiv_dispc_clk;
 	dsi->current_cinfo.dsi_pll_hsdiv_dsi_clk =
@@ -1482,12 +1484,12 @@ int dsi_pll_set_clock_div(struct platform_device *dsidev,
 			cinfo->regm,
 			cinfo->regn,
 			cinfo->clkin,
-			cinfo->clkin4ddr);
+			cinfo->clkinxddr);
 
 	DSSDBG("Data rate on 1 DSI lane %ld Mbps\n",
-			cinfo->clkin4ddr / 1000 / 1000 / 2);
+			(cinfo->clkinxddr / 1000 / 1000 / dsi->ddr_div) * 2);
 
-	DSSDBG("Clock lane freq %ld Hz\n", cinfo->clkin4ddr / 4);
+	DSSDBG("Clock lane freq %ld Hz\n", cinfo->clkinxddr / dsi->ddr_div);
 
 	DSSDBG("regm_dispc = %d, %s (%s) = %lu\n", cinfo->regm_dispc,
 		dss_get_generic_clk_source_name(OMAP_DSS_CLK_SRC_DSI_PLL_HSDIV_DISPC),
@@ -1533,7 +1535,7 @@ int dsi_pll_set_clock_div(struct platform_device *dsidev,
 	}
 
 	if (dss_has_feature(FEAT_DSI_PLL_SELFREQDCO))
-		f = cinfo->clkin4ddr < 1000000000 ? 0x2 : 0x4;
+		f = cinfo->clkinxddr < 1000000000 ? 0x2 : 0x4;
 
 	l = dsi_read_reg(dsidev, DSI_PLL_CONFIGURATION2);
 
@@ -1703,8 +1705,8 @@ static void dsi_dump_dsidev_clocks(struct platform_device *dsidev,
 
 	seq_printf(s,	"Fint\t\t%-16luregn %u\n", cinfo->fint, cinfo->regn);
 
-	seq_printf(s,	"CLKIN4DDR\t%-16luregm %u\n",
-			cinfo->clkin4ddr, cinfo->regm);
+	seq_printf(s,	"CLKIN%dDDR\t%-16luregm %u\n",
+			dsi->ddr_div, cinfo->clkinxddr, cinfo->regm);
 
 	seq_printf(s,	"DSI_PLL_HSDIV_DISPC (%s)\t%-16luregm_dispc %u\t(%s)\n",
 			dss_feat_get_clk_source_name(dsi_module == 0 ?
@@ -1733,7 +1735,7 @@ static void dsi_dump_dsidev_clocks(struct platform_device *dsidev,
 	seq_printf(s,	"DSI_FCLK\t%lu\n", dsi_fclk_rate(dsidev));
 
 	seq_printf(s,	"DDR_CLK\t\t%lu\n",
-			cinfo->clkin4ddr / 4);
+			cinfo->clkinxddr / dsi->ddr_div);
 
 	seq_printf(s,	"TxByteClkHS\t%lu\n", dsi_get_txbyteclkhs(dsidev));
 
@@ -2073,7 +2075,7 @@ static inline unsigned ns2ddr(struct platform_device *dsidev, unsigned ns)
 	struct dsi_data *dsi = dsi_get_dsidrv_data(dsidev);
 
 	/* convert time in ns to ddr ticks, rounding up */
-	unsigned long ddr_clk = dsi->current_cinfo.clkin4ddr / 4;
+	unsigned long ddr_clk = dsi->current_cinfo.clkinxddr / dsi->ddr_div;
 	return (ns * (ddr_clk / 1000 / 1000) + 999) / 1000;
 }
 
@@ -2081,7 +2083,7 @@ static inline unsigned ddr2ns(struct platform_device *dsidev, unsigned ddr)
 {
 	struct dsi_data *dsi = dsi_get_dsidrv_data(dsidev);
 
-	unsigned long ddr_clk = dsi->current_cinfo.clkin4ddr / 4;
+	unsigned long ddr_clk = dsi->current_cinfo.clkinxddr / dsi->ddr_div;
 	return ddr * 1000 * 1000 / (ddr_clk / 1000);
 }
 
@@ -4906,6 +4908,8 @@ static int __init omap_dsihw_probe(struct platform_device *dsidev)
 		return r;
 
 	pm_runtime_enable(&dsidev->dev);
+
+	dsi->ddr_div = dss_feat_get_dsi_ddr_div();
 
 	r = dsi_runtime_get(dsidev);
 	if (r)

andygross/omap_dmm_tiler

Signed-off-by: Tomi Valkeinen <tomi.valkeinen@ti.com>

drivers/video/omap2/dss/dsi.c
@@ -325,6 +325,8 @@ struct dsi_data {
 	unsigned long  fint_min, fint_max;
 	unsigned long lpdiv_max;
 
+	int ddr_div;
+
 	unsigned num_lanes_supported;
 
 	struct dsi_lane_config lanes[DSI_MAX_NR_LANES];
@@ -1179,7 +1181,7 @@ static unsigned long dsi_get_txbyteclkhs(struct platform_device *dsidev)
 {
 	struct dsi_data *dsi = dsi_get_dsidrv_data(dsidev);
 
-	return dsi->current_cinfo.clkin4ddr / 16;
+	return dsi->current_cinfo.clkin4ddr / (dsi->ddr_div * 4);
 }
 
 static unsigned long dsi_fclk_rate(struct platform_device *dsidev)
@@ -1474,7 +1476,7 @@ static int dsi_pll_calc_ddrfreq(struct platform_device *dsidev,
 
 	cur.clkin = dss_sys_clk;
 
-	req_clkin4ddr = req_clk * 4;
+	req_clkin4ddr = req_clk * dsi->ddr_div;
 
 	for (cur.regn = 1; cur.regn < dsi->regn_max; ++cur.regn) {
 		cur.fint = cur.clkin / cur.regn;
@@ -1553,9 +1555,9 @@ int dsi_pll_set_clock_div(struct platform_device *dsidev,
 			cinfo->clkin4ddr);
 
 	DSSDBG("Data rate on 1 DSI lane %ld Mbps\n",
-			cinfo->clkin4ddr / 1000 / 1000 / 2);
+			(cinfo->clkin4ddr / 1000 / 1000 / dsi->ddr_div) * 2);
 
-	DSSDBG("Clock lane freq %ld Hz\n", cinfo->clkin4ddr / 4);
+	DSSDBG("Clock lane freq %ld Hz\n", cinfo->clkin4ddr / dsi->ddr_div);
 
 	DSSDBG("regm_dispc = %d, %s (%s) = %lu\n", cinfo->regm_dispc,
 		dss_get_generic_clk_source_name(OMAP_DSS_CLK_SRC_DSI_PLL_HSDIV_DISPC),
@@ -1768,8 +1770,8 @@ static void dsi_dump_dsidev_clocks(struct platform_device *dsidev,
 
 	seq_printf(s,	"Fint\t\t%-16luregn %u\n", cinfo->fint, cinfo->regn);
 
-	seq_printf(s,	"CLKIN4DDR\t%-16luregm %u\n",
-			cinfo->clkin4ddr, cinfo->regm);
+	seq_printf(s,	"CLKIN%dDDR\t%-16luregm %u\n",
+			dsi->ddr_div, cinfo->clkin4ddr, cinfo->regm);
 
 	seq_printf(s,	"DSI_PLL_HSDIV_DISPC (%s)\t%-16luregm_dispc %u\t(%s)\n",
 			dss_feat_get_clk_source_name(dsi_module == 0 ?
@@ -1798,7 +1800,7 @@ static void dsi_dump_dsidev_clocks(struct platform_device *dsidev,
 	seq_printf(s,	"DSI_FCLK\t%lu\n", dsi_fclk_rate(dsidev));
 
 	seq_printf(s,	"DDR_CLK\t\t%lu\n",
-			cinfo->clkin4ddr / 4);
+			cinfo->clkin4ddr / dsi->ddr_div);
 
 	seq_printf(s,	"TxByteClkHS\t%lu\n", dsi_get_txbyteclkhs(dsidev));
 
@@ -2138,7 +2140,7 @@ static inline unsigned ns2ddr(struct platform_device *dsidev, unsigned ns)
 	struct dsi_data *dsi = dsi_get_dsidrv_data(dsidev);
 
 	/* convert time in ns to ddr ticks, rounding up */
-	unsigned long ddr_clk = dsi->current_cinfo.clkin4ddr / 4;
+	unsigned long ddr_clk = dsi->current_cinfo.clkin4ddr / dsi->ddr_div;
 	return (ns * (ddr_clk / 1000 / 1000) + 999) / 1000;
 }
 
@@ -2146,7 +2148,7 @@ static inline unsigned ddr2ns(struct platform_device *dsidev, unsigned ddr)
 {
 	struct dsi_data *dsi = dsi_get_dsidrv_data(dsidev);
 
-	unsigned long ddr_clk = dsi->current_cinfo.clkin4ddr / 4;
+	unsigned long ddr_clk = dsi->current_cinfo.clkin4ddr / dsi->ddr_div;
 	return ddr * 1000 * 1000 / (ddr_clk / 1000);
 }
 
@@ -4214,7 +4216,8 @@ int omapdss_dsi_set_clocks(struct omap_dss_device *dssdev,
 
 	/* pck = TxByteClkHS * datalanes * 8 / bitsperpixel */
 
-	pck = cinfo.clkin4ddr / 16 * (dsi->num_lanes_used - 1) * 8 / bpp;
+	pck = cinfo.clkin4ddr / (dsi->ddr_div * 4) *
+		(dsi->num_lanes_used - 1) * 8 / bpp;
 
 	DSSDBG("finding dispc dividers for pck %lu\n", pck);
 
@@ -5216,6 +5219,8 @@ static int __init omap_dsihw_probe(struct platform_device *dsidev)
 		return r;
 
 	pm_runtime_enable(&dsidev->dev);
+
+	dsi->ddr_div = dss_feat_get_dsi_ddr_div();
 
 	r = dsi_runtime_get(dsidev);
 	if (r)